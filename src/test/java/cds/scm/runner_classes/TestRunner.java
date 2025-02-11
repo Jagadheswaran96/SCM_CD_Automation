88e32e8ae82d8cf4aa1b5b75eec27fb2a48fb59a
Jagadheswaran <jagadhesjack88@gmail.com>
2025-02-12 00:07:29 +0530

    added description for runner class

--- src/test/java/cds/scm/runner_classes/TestRunner.java
+++ src/test/java/cds/scm/runner_classes/TestRunner.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+//Runner class
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/features",
